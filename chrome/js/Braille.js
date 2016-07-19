63513b692b2db31a88be1ae6baebcb169878cdec
Anwar3746 <Anwar3746@gmail.com>
2016-07-19 16:32:28 +0530

    chrome plugin made working

--- chrome/js/Braille.js
+++ chrome/js/Braille.js
@@ -1,9 +1,27 @@
 (function() {
 	var isHovering=false;
 	
+	var current_language = 0;
+	
+	var pressedKeys = "";
+
+	var simple_mode = 0;
+
+	var compleate_languages_map = [];
+
+	var compleate_abbreviations_map = [];
+
+	var compleate_contractions_dict = [];
+
+	var keycode_map = {"1":"70","2":"68","3":"83","4":"74","5":"75","6":"76","7":"65","8":"71","9":"72","0":"186","0":"59","-1":"18"};
+	
+	var braille_letter_map_pos = 0;
+	
+	var caps_lock = 0;
+	
 	chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 		if (request.action == "LanguageSelect") {
-			initVarnam(request.language, request.server);
+			initVarnam(request);
 		} 
 		else if (request.action == 'VarnamEnable') {
 			if(!request.enable){
@@ -13,7 +31,7 @@
 					alert("Error while enabling braille. Default language is not set. Click on the language name or set a default language from the preferences screen (Menu -> Settings -> Extensions -> Varnam -> Options) before enabling braille");
 					return;
 				}
-				initVarnam(request.language, request.server);
+				initVarnam(request);
 			}
 		}
 	});
@@ -38,12 +56,18 @@
 
 
 
-	function initVarnam(data, server) {
+	function initVarnam(request) {
 		var active = window.document.activeElement;
 		if (active) {
-			$(active).data('braille-lang', data);
-			$(active).data('braille-server', server);
-			$(active).data('braille-input-value', active.value);
+			//$(active).data('braille-lang', data);
+			current_language = request.language;
+			compleate_languages_map = request.map;
+			keycode_map = request.key_map;
+			compleate_abbreviations_map = request.a_map;
+			compleate_contractions_dict = request.c_dict;    
+			simple_mode = request.s_mode;
+
+			//$(active).data('braille-input-value', active.value);
 
 			$(active).off('keydown', key_press);
 			$(active).on('keydown', key_press);
@@ -69,45 +93,177 @@
 		return strA.toLowerCase().localeCompare(strB.toLowerCase());
 	}
 
+function case_insensitive_comp(strA, strB) {
+ 	return strA.toLowerCase().localeCompare(strB.toLowerCase());
+}
+
+function insertAtCaret(txtarea,text) {
+	var scrollPos = txtarea.scrollTop;
+	var strPos = 0;
+	strPos = txtarea.selectionStart;
+	var front = (txtarea.value).substring(0,strPos);
+	var back = (txtarea.value).substring(strPos,txtarea.value.length);
+	txtarea.value=front+text+back;
+	strPos = strPos + text.length;
+	txtarea.selectionStart = strPos;
+	txtarea.selectionEnd = strPos;
+	txtarea.focus();
+	txtarea.scrollTop = scrollPos;
+}
+
+function setCaretPosition(elem, caretPos) {
+	if(elem != null) {
+		if(elem.createTextRange) {
+			var range = elem.createTextRange();
+			range.move('character', caretPos);
+			range.select();
+		}
+		else {
+			if(elem.selectionStart) {
+				elem.focus();
+				elem.setSelectionRange(caretPos, caretPos);
+			}
+			else
+				elem.focus();
+		}
+	}
+}
+
+
 function key_release(event) {
 	//var event = $.event.fix(e);
-	activeElement = event.target;
-	console.log("Key Released"+activeElement.items);
-	if(activeElement.items != ""){
-		activeElement.items = activeElement.items.split("");
-		activeElement.items = activeElement.items.sort( case_insensitive_comp )
-		activeElement.items = activeElement.items.join("");
-		console.log(activeElement.items);		
+	var activeElement = event.target;
+	if(pressedKeys != ""){
+		pressedKeys = pressedKeys.split("");
+		pressedKeys = pressedKeys.sort( case_insensitive_comp )
+		pressedKeys = pressedKeys.join("");
+		//console.log(activeElement.items);
+		console.log(pressedKeys);
 
-		if(activeElement.items == "F"){
-			activeElement.value = activeElement.value+"a";
-		}	
-		if(activeElement.items == "DF"){
-			activeElement.value = activeElement.value+"b";
+		//Move map position to contraction if any
+		console.log(compleate_contractions_dict[current_language]);
+		if  ( pressedKeys in compleate_contractions_dict[current_language]){
+			braille_letter_map_pos = compleate_contractions_dict[current_language][pressedKeys];
+			console.log("Switching to map position "+ braille_letter_map_pos);
 		}
-		if(activeElement.items == "FS"){
-			activeElement.value = activeElement.value+"k";
-		}
-		if(activeElement.items == "DFS"){
-			activeElement.value = activeElement.value+"l";
-		}
-		if(activeElement.items == "FJ"){
-			activeElement.value = activeElement.value+"c";
-		}
-		if(activeElement.items == " ")
+
+		if (pressedKeys == "-1")
 		{
-			activeElement.value = activeElement.value+" ";
-		}	
+			if (braille_letter_map_pos != 0)
+				braille_letter_map_pos = 0;
+			else
+				braille_letter_map_pos = 1;
+			console.log(braille_letter_map_pos);
+		}
+
+		if (pressedKeys == "0")
+		{
+			braille_letter_map_pos = 2;
+		}
+
+		if (pressedKeys == "8")
+		{
+			console.log(!caps_lock);
+			caps_lock = !caps_lock;
+		}
+
+		if(pressedKeys == "9")
+		{
+			var textAreaTxt = activeElement.value;
+			var iCaretPos = activeElement.selectionStart
+			var left = textAreaTxt.slice(0,iCaretPos-1);
+			var right = textAreaTxt.slice(iCaretPos,(textAreaTxt.length));
+			activeElement.value = left+right
+			setCaretPosition(activeElement,iCaretPos-1);
+		}
+
+		if(pressedKeys == "89")
+		{
+			var textAreaTxt = activeElement.value;
+			var iCaretPos = activeElement.selectionStart;
+			var words = textAreaTxt.slice(0,iCaretPos).split(" ");
+			var last_word = words.slice(-1)[0];
+			var left = textAreaTxt.slice(0,iCaretPos-(last_word.length));
+			var right = textAreaTxt.slice(iCaretPos,(textAreaTxt.length));
+			activeElement.value = left+right
+			setCaretPosition(activeElement,iCaretPos-(last_word.length));
+		}
+
+		// ##########################################
+			//Abbreviation expansion
+		if (pressedKeys == "7" && simple_mode == 0)
+		{
+			try
+			{
+				var textAreaTxt = activeElement.value;
+				var iCaretPos = activeElement.selectionStart;
+				var words = textAreaTxt.slice(0,iCaretPos).split(" ");
+				var last_word = words.slice(-1)[0];
+
+				if (compleate_abbreviations_map[current_language][last_word.toLowerCase()])
+				{
+					console.log(compleate_abbreviations_map[current_language][last_word.toLowerCase()]);
+					var left = textAreaTxt.slice(0,iCaretPos-(last_word.length));
+					var right = textAreaTxt.slice(iCaretPos,(textAreaTxt.length));
+
+					if(caps_lock)
+						activeElement.value = (left+compleate_abbreviations_map[current_language][last_word].toUpperCase()+right)
+					else
+						activeElement.value = (left+compleate_abbreviations_map[current_language][last_word]+right)
+
+					if(right.length > 0)
+						setCaretPosition(activeElement,iCaretPos+(compleate_abbreviations_map[current_language][last_word].length-1));
+					else
+						setCaretPosition(activeElement,iCaretPos+(compleate_abbreviations_map[current_language][last_word].length));
+
+					braille_letter_map_pos = 1;
+				}
+			}
+			catch(e)
+			{
+				console.log(e);
+			}
+		}
+
+
+		try
+		{
+			if (compleate_languages_map[current_language][pressedKeys][braille_letter_map_pos])
+			{
+				console.log(pressedKeys,compleate_languages_map[current_language][pressedKeys][braille_letter_map_pos]);
+				if(caps_lock)
+					insertAtCaret(activeElement,compleate_languages_map[current_language][pressedKeys][braille_letter_map_pos].toUpperCase());
+				else
+					insertAtCaret(activeElement,compleate_languages_map[current_language][pressedKeys][braille_letter_map_pos]);
+				braille_letter_map_pos = 1;
+			}
+
+		}
+		catch(e)
+		{
+			console.log(e);
+		}
+
 	}
-	activeElement.items="";
+	pressedKeys = "";
 	event.preventDefault();	
 }
 
 function key_press(event) {
-	//var event = $.event.fix(e);
-	activeElement = event.target;
-	activeElement.items=activeElement.items+String.fromCharCode(event.keyCode)
-	console.log("Key pressed");
+	// 32 - Space
+	console.log(event.keyCode);
+	if(event.keyCode == 32){
+	braille_letter_map_pos = 0;
+	insertAtCaret(event.target," ");
+	}
+
+	for (var key in keycode_map) {
+		if (keycode_map.hasOwnProperty(key)) {
+			if(keycode_map[key] == event.keyCode){
+				pressedKeys=pressedKeys+key;
+			}
+		}
+	}
 	event.preventDefault();
 }
 
--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,3 +1,24 @@
+// The map used for transilation
+var map = {};
+
+//Contraction dict
+var contractions_dict = {};
+
+//Abbreviations dict
+var abbreviations = {};
+
+//Temporory
+var simple_mode = 0;
+
+var compleate_languages_map = [];
+
+var compleate_abbreviations_map = [];
+
+var compleate_contractions_dict = [];
+
+var keycode_map = {"1":"70","2":"68","3":"83","4":"74","5":"75","6":"76","7":"65","8":"71","9":"72","0":"186","0":"59","-1":"18"};
+
+
 var BrailleMenu = chrome.contextMenus.create({
     "title": "Braille",
     "contexts": ["editable"]
@@ -19,21 +40,25 @@ var separator = chrome.contextMenus.create({
     contexts: ["editable"]
 });
 
-var hindi = chrome.contextMenus.create({
-    "title": "Hindi",
-    "parentId": BrailleMenu,
-    "id": "Braille_hi",
-    "contexts": ["editable"],
-    "onclick": handleLanguageSelection
-});
+var req = new XMLHttpRequest();
+req.open("GET", chrome.extension.getURL("braille/languages.txt"),false);
+req.send();
 
-var malayalam = chrome.contextMenus.create({
-    "title": "Malayalam",
-    "parentId": BrailleMenu,
-    "id": "Braille_ml",
-    "contexts": ["editable"],
-    "onclick": handleLanguageSelection
-});
+var available_languages = [];
+var lines = req.response.split("\n");
+for (var i = 0, len = lines.length; i < len; i++) {
+	  if (lines[i] != ""){
+		available_languages.push(lines[i].split("-")[0]);
+		var lang = chrome.contextMenus.create({
+			"title": lines[i].split("-")[0],
+			"parentId": BrailleMenu,
+			"id": "Braille_"+lines[i].split("-")[0],
+			"contexts": ["editable"],
+			"onclick": handleLanguageSelection
+		});
+	}
+}
+
 
 function handleLanguageSelection(info, tab) {
     var lang = info.menuItemId.replace("Braille_", "");
@@ -43,8 +68,12 @@ function handleLanguageSelection(info, tab) {
   }
     chrome.tabs.sendMessage(tab.id, {
         action: "LanguageSelect",
-        language: lang,
-        server: BrailleServer()
+        language: available_languages.indexOf(lang),
+        map : compleate_languages_map,
+        key_map : keycode_map,
+        a_map : compleate_abbreviations_map,
+        c_dict : compleate_contractions_dict,        
+        s_mode : simple_mode        
     });
 }
 
@@ -76,3 +105,107 @@ function(request, sender, sendResponse) {
     break;
     }
 });
+
+
+
+
+
+
+
+
+function load_all_languages()
+{
+	
+	for (var i = 0, len = available_languages.length; i < len; i++) {
+		load_language(available_languages[i].split("-")[0]);
+		compleate_languages_map.push(map);
+		compleate_abbreviations_map.push(abbreviations);
+		compleate_contractions_dict.push(contractions_dict);
+	}
+}
+
+load_all_languages()
+//alert(compleate_languages_map[0]["12"]);
+
+function load_language(language){
+	//console.log("loading Map for language : %s" + language)
+	map = {};
+	contractions_dict = {};
+
+	var submap_number = 1;
+	append_sub_map("beginning.txt",submap_number,language);
+
+	submap_number = 2;
+	append_sub_map("middle.txt",submap_number,language);
+
+	submap_number = 3;
+	append_sub_map("punctuations.txt",submap_number,language);
+
+	if (simple_mode == 0)
+	{
+		var req_contraction_list = new XMLHttpRequest();
+		req_contraction_list.open("GET", chrome.extension.getURL("braille/"+language+"/contraction_map_list.txt"),false);
+		req_contraction_list.send();
+		var files = req_contraction_list.response.split("\n");
+		for (var i = 0, len = files.length; i < len; i++) {
+			if (files[i] != "" && simple_mode == 0){
+				submap_number += 1;
+				append_sub_map(files[i],submap_number,language);
+				contractions_dict[files[i].slice(0, -4)] = submap_number-1;
+			}
+		}
+
+		//Load abbreviations if exist
+		load_abbrivation(language);
+	}
+}
+
+
+function append_sub_map(filename,submap_number,language){
+	//console.log("Loading sub map file for : " +language+"/"+filename+"On submap : "+submap_number)
+	var req = new XMLHttpRequest();
+	req.open("GET", chrome.extension.getURL("braille/"+language+"/"+filename),false);
+	req.send();
+	var lines = req.response.split("\n");
+	for (var i = 0, len = lines.length; i < len; i++) {
+		if (lines[i] != ""){
+			if ((lines[i].split(" ")[0]) in map)
+			{
+				map[lines[i].split(" ")[0]].push(lines[i].split(" ")[1]) // may be new line to be removed
+				//if (map[lines[i].split(" ")[0]].length != submap_number)
+				//	console.log("Repeated on : ",lines[i].split(" ")[0])
+			}
+			else{
+				var list = [];
+				for (var j=1; j<submap_number; j++) {
+					list.push(" ");
+				}
+				list.push(lines[i].split(" ")[1]); // may be new line to be removed
+				map[lines[i].split(" ")[0]] = list;
+			}
+		}
+	}
+
+	//Fill blank if empty
+	for(var key in map) {
+		var value = map[key];
+		if(value.length < submap_number)
+		{
+			map[key].push("");
+		}
+	}
+}
+
+function load_abbrivation(language){
+	abbreviations = {}
+	var req_abbrivation = new XMLHttpRequest();
+	req_abbrivation.open("GET", chrome.extension.getURL("braille/"+language+"/abbreviations.txt"),false);
+	req_abbrivation.send();
+	var lines = req_abbrivation.response.split("\n");
+	for (var i = 0, len = lines.length; i < len; i++) {
+		if (lines[i] != ""){
+			abbreviations[lines[i].split("  ")[0]] = lines[i].split("  ")[1];
+		}
+	}
+}
+
